6d9b9816022c57b3d12638b16aa35b2fb98d3e27
yugsharma4 <yugsharma422@gmail.com>
2023-01-15 21:10:38 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { Box } from "@mui/material";
 function App() {
-  return <div className="App">Yug Sharma</div>;
+  return <Box sx={{ color: "red" }}>Yug Sharma</Box>;
 }
 
 export default App;
